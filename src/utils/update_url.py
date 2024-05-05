3515c95ef8a2509c91bf25635d77ab012ccad819
mlzlzj <161783521+mlzlzj@users.noreply.github.com>
2024-05-05 19:09:26 +0800

    Update update_url.py

--- src/utils/update_url.py
+++ src/utils/update_url.py
@@ -4,12 +4,8 @@ import sys # 接受外部传参
 
 # 发送HTTP请求并获取JSON数据
 def get_live_url(channel_id):
-    proxy    {
-    http':'60.174.212.8089
-    http':'117.70.49.11.8089
-    }
     url = f'http://mpp.liveapi.mgtv.com/v1/epg/turnplay/getLivePlayUrlMPP?version=PCweb_1.0&platform=1&buss_id=2000001&channel_id={channel_id}'
-    # print(channel_id, url)
+    print(channel_id, url)
     response = requests.get(url)
     data = response.json()
     return data.get('data', {}).get('url')
@@ -36,7 +32,7 @@ print(live_url)
 
 if live_url:
     update_single_m3u8_file(live_url, filename)
-    # print('文件 {filename} 的URL已更新为：')
-    # print(f'{live_url}\n')
+    print('文件 {filename} 的URL已更新为：')
+    print(f'{live_url}\n')
 else:
     print('未能获取到直播URL, 请检查网络或参数设置。')
