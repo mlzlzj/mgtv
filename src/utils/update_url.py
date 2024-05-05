029c1ecea782b3c517ac843d25f04ae7cfddc6ac
mlzlzj <161783521+mlzlzj@users.noreply.github.com>
2024-05-05 19:06:50 +0800

    Update update_url.py

--- src/utils/update_url.py
+++ src/utils/update_url.py
@@ -4,6 +4,10 @@ import sys # 接受外部传参
 
 # 发送HTTP请求并获取JSON数据
 def get_live_url(channel_id):
+    proxy    {
+    http':'60.174.212.8089
+    http':'117.70.49.11.8089
+    }
     url = f'http://mpp.liveapi.mgtv.com/v1/epg/turnplay/getLivePlayUrlMPP?version=PCweb_1.0&platform=1&buss_id=2000001&channel_id={channel_id}'
     # print(channel_id, url)
     response = requests.get(url)
